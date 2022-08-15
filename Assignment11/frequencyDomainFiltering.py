2a631ee3f21cc7293a04ea0229bd4db2c107694e
Shahriar Alif <shahriarcse26@gmail.com>
2022-08-15 16:13:00 +0600

    Assignment11 completed

--- Assignment11/frequencyDomainFiltering.py
+++ Assignment11/frequencyDomainFiltering.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def main():
 	# Load image.
-	img_path = '/home/bibrity/Image_with_Python/Rose.jpeg'#GhostPepper.jpg' #PaddyField.jpeg'
+	img_path = 'images/cube.jpg'#GhostPepper.jpg' #PaddyField.jpeg'
 	rgb = plt.imread(img_path)
 
 	# Convert images
@@ -16,40 +16,37 @@ def main():
 	magnitude_spectrum = 100 * np.log(np.abs(ftimg))
 	centered_magnitude_spectrum = 100 * np.log(np.abs(centered_ftimg))
 
-	print(gray.shape, ftimg.shape, centered_ftimg.shape)
-	print(gray.max(), gray.min(), ftimg.max(), ftimg.min(), centered_ftimg.max(), centered_ftimg.min())
+	width,height = gray.shape
+	black = np.zeros((width,height), dtype = 'uint8')
+	cv2.circle(black, (black.shape[1]//2 , black.shape[0]//2), 80, (1,1,1), thickness=-1)
 
-	# Build a Gaussian filter.
-	ncols, nrows = gray.shape
-	gaussian_filter = build_gaussian_filter(ncols, nrows)
+	gaussian = cv2.GaussianBlur(gray,(5,5),cv2.BORDER_DEFAULT)
 
+	gaussian_mask = gaussian*black
+	
+	
 	# Apply Gaussian filter
-	ftimg_gf = centered_ftimg * gaussian_filter
-	filtered_img = np.abs(np.fft.ifft2(ftimg_gf))
+	ftimg_gf1 = centered_ftimg * gaussian_mask
+	filtered_img1 = np.abs(np.fft.ifft2(ftimg_gf1))
+
+	kernel = np.array([[2, 0, -2], [1, 0, -1], [2, 0, -2]])
+	Filter1 = cv2.filter2D(gray,-1,kernel)
+	filter_mask1 = Filter1 * black
+
+	ftimg_gf2 = centered_ftimg * filter_mask1
+	filtered_img2 = np.abs(np.fft.ifft2(ftimg_gf2))
 
 	# Save images all together by matplotlib.
-	img_set = [rgb, gray, magnitude_spectrum, centered_magnitude_spectrum, gaussian_filter, filtered_img]
-	title_set = ['RGB', 'Gray', 'FFT2', 'Centered FFT2', 'Gaussian Filter', 'Filtered Img']
-	figpath = '/home/bibrity/DigitalImageProcessing/FFT2.png'
-	matplotlib_plot_img(img_set, title_set, figpath)
-
-def build_gaussian_filter(ncols, nrows):
-	sigmax, sigmay = 10, 10
-	cx, cy = nrows/2, ncols/2
-	x = np.linspace(0, nrows, nrows)
-	y = np.linspace(0, ncols, ncols)
-	X, Y = np.meshgrid(x, y)
-	gaussian_filter = np.exp(-(((X-cx)/sigmax)**2 + ((Y-cy)/sigmay)**2))
-	print(gaussian_filter.shape)
-
-	return gaussian_filter
+	img_set = [rgb, gray, magnitude_spectrum, centered_magnitude_spectrum, gaussian_mask, filtered_img1, filter_mask1,filtered_img2]
+	title_set = ['RGB', 'Gray', 'FFT2', 'Centered FFT2', 'Gaussian Filter', 'Filtered Img', "Random Filter", "Random Effect"]
+	matplotlib_plot_img(img_set, title_set)
 	
-def matplotlib_plot_img(img_set, title_set, figpath):
-	plt.rcParams.update({'font.size': 22})			
-	plt.figure(figsize = (20, 20))
+def matplotlib_plot_img(img_set, title_set):
+	plt.rcParams.update({'font.size': 16})			
+	plt.figure(figsize = (10, 6))
 	n = len(img_set)
 	for i in range(n):
-		plt.subplot(2, 3, i + 1)
+		plt.subplot(2, 4, i + 1)
 		plt.title(title_set[i])
 		img = img_set[i]
 		ch = len(img.shape)
@@ -59,8 +56,7 @@ def matplotlib_plot_img(img_set, title_set, figpath):
 			plt.imshow(img)			
 
 	plt.tight_layout()
-	plt.savefig(figpath)	
-	plt.show()
+	plt.savefig("output.png")
 
 if __name__ == '__main__':
 	main()
